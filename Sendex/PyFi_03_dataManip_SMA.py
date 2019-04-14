7914ede0c26208fffc4dbea1d69cbdc60c655296
Oliver Oliverio <oliveroliverio@gmail.com>
2019-04-14 12:54:16 -0700

    pyfi 3

--- Sendex/PyFi_03_dataManip_SMA.py
+++ Sendex/PyFi_03_dataManip_SMA.py
@@ -8,11 +8,10 @@ import pandas as pd
 import pandas_datareader.data as web
 import time
 import os
-
 style.use('ggplot')
 
-#--------timed explanation functions--------
-#---------------------------------------------
+#--------timed explanation functions--------------------
+#-------------------------------------------------------
 def timedMessage(message):
  #   time.sleep(1)
     print("\n")
@@ -23,38 +22,45 @@ def timedMessage(message):
     # I want to clear the console screen but the bottom doesn't work
     #os.system('clear')
 
-
-# ----Convert DF to CSV-----------
-timedMessage("Convert DF to CSV")
-# start = dt.datetime(2015,1,1)
-# end = dt.datetime(2019,4,14)
-# df = web.DataReader('TSLA', 'yahoo', start,end)
-# # convert above df into csv
-# df.to_csv('tsla.csv')
-
-
-#---Read a CSV in-----
-timedMessage("here's how to read in CSV from local")
-df = pd.read_csv('tsla.csv')
-print(df.head())
-
-# However, we want a datetime index
-# let the 1st col be the index w/ index_col
-timedMessage("However, we want a datetime index")
+# read in data
 df = pd.read_csv('tsla.csv', parse_dates=True, index_col=0)
+
+# make new MA column
+df['100ma'] = df['Adj Close'].rolling(window=100).mean()
+
+# drop NAs
+df.dropna(inplace=True)
+
+# change the df.MA to below, so that it won't require any number of periods to calculate MA
+# it'll just be whatever the current price is
+df['100ma'] = df['Adj Close'].rolling(window=100, min_periods=0).mean()
+
 print(df.head())
-timedMessage("be sure to lookup how to read json")
 
-#---------Plot------------
-timedMessage("DF's have plot attributes, so try the function df.plot(), df.show()")
-df.plot()
+# plot MA overlayed w/ price
+# making plots on top of plots...?
+# take grid size, 6 rows, 1 col.  the start point is 0,0
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1)
+
+# now that these subplots are generated, we can graph to them
+# plot simple line, we have x axis as date, which is the index
+# the y axis is the adj close, then another line for 100ma
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+# make another plot (actually bars) below as volume
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
 
-# only want one column
-timedMessage("say we only want adjusted close column, plot this")
-df['Adj Close'].plot()
+# quick change
+timedMessage("quick change, modify ax2 to sharex=ax1, so that when you zoom, both plots align")
+ax1 = plt.subplot2grid((6,1), (0,0), rowspan=5, colspan=1)
+ax2 = plt.subplot2grid((6,1), (5,0), rowspan=1, colspan=1, sharex=ax1)
+ax1.plot(df.index, df['Adj Close'])
+ax1.plot(df.index, df['100ma'])
+ax2.bar(df.index, df['Volume'])
+
 plt.show()
 
 
-#----------subsetting DFs
-print(df[['Open', 'High']].head())
