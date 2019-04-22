d6755050a85054f7044d0e5d7373559713029b85
Oliver Oliverio <oliveroliverio@gmail.com>
2019-04-21 18:56:34 -0700

    added some code

--- _MyStuff/PyOCR/PyOCR.py
+++ _MyStuff/PyOCR/PyOCR.py
@@ -1,6 +1,19 @@
 # documentation: https://pypi.org/project/pyscreenshot/
 # need to install this on all python runners
 
-import pyscreenshot
+import pyscreenshot as ImageGrab
+import time
+import pytesseract as pt
 
-im = ImageGrab.grab()
+# wait 2 sec for you to pull up the image you want to store
+time.sleep(2)
+
+# store screen shot in variable 'im'
+im = ImageGrab.grab(bbox=(10, 10, 510, 510))  # X1,Y1,X2,Y2
+
+# send 'im' to OCR
+text = pt.image_to_string(im)
+
+print(text)
+
+
